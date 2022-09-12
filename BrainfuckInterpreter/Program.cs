b7004016a4ce41998493f8dc3528e7e42bad1bd5
Rafael Scholz Griebler <rafaelscholz03@gmail.com>
2022-09-12 00:23:57 -0300

    Initial commit - create project and interpreter with default operations support

---
+++ BrainfuckInterpreter/Program.cs
@@ -0,0 +1,90 @@
+﻿if (args.Length == 0)
+{
+    throw new ArgumentException("Must provide path to program file as the first argument.");
+}
+
+var sourcePath = args[0];
+
+var source = File.OpenText(sourcePath);
+var buffer = source.ReadToEnd();
+
+var memory = new int[30000];
+var pointer = 0;
+
+for (var i = 0; i < buffer.Length; i++)
+{
+    var token = buffer[i];
+
+    int innerBrackets;
+    switch (token)
+    {
+        case '>':
+            pointer++;
+            break;
+            
+        case '<':
+            pointer--;
+            break;
+            
+        case '+':
+            memory[pointer]++;
+            break;
+            
+        case '-':
+            memory[pointer]--;
+            break;
+            
+        case '.':
+            var text = (char)memory[pointer];
+            Console.Write(text);
+            break;
+            
+        case ',':
+            var input = Console.ReadLine() ?? string.Empty;
+            var character = input.ToCharArray().ElementAtOrDefault(0);
+            memory[pointer] = character;
+            break;
+            
+        case '[':
+            if (memory[pointer] != 0) break;
+            innerBrackets = 0;
+            for (var j = i + 1; j < buffer.Length; j++)
+            {
+                if (buffer[j] == ']' && innerBrackets == 0)
+                {
+                    i = j;
+                    break;
+                }
+                else if (buffer[j] == ']' && innerBrackets > 0)
+                {
+                    innerBrackets--;
+                }
+                else if (buffer[j] == '[')
+                {
+                    innerBrackets++;
+                }
+            }
+            break;
+        
+        case ']':
+            if (memory[pointer] == 0) break;
+            innerBrackets = 0;
+            for (var j = i - 1; j >= 0; j--)
+            {
+                if (buffer[j] == '[' && innerBrackets == 0)
+                {
+                    i = j;
+                    break;
+                }
+                else if (buffer[j] == '[' && innerBrackets > 0)
+                {
+                    innerBrackets--;
+                }
+                else if (buffer[j] == ']')
+                {
+                    innerBrackets++;
+                }
+            }
+            break;
+    }
+}
